f88a0ed3f024d8898311253a226aa8c8d1844770
Mohamed Debbagh <moha7108@protonmail.com>
2023-06-29 14:37:33 -0400

    Update class from motor to the driver number, so that it includes 2 motores, update test code for 2 drivers 4 motores

--- motor_control.cpp
+++ motor_control.cpp
@@ -3,91 +3,149 @@
 #include <string>
 using namespace std;
 
-class Motor {
+class TB6612FNG_MD {
     public:
-        Motor(uint PWM_pin, uint forward_pin, uint reverse_pin, uint stby_pin) 
-        : pwm_pin(PWM_pin), dir_pin1(forward_pin), dir_pin2(reverse_pin), stby_pin(stby_pin){
+        TB6612FNG_MD(uint PWMA_pin=0, uint AI2_pin=1, uint AI1_pin=2, uint STBY_pin=6, uint BI1_pin =5, uint BI2_pin=4, uint PWMB_pin=3) 
+        : pwma_pin(PWMA_pin), ai2_pin(AI2_pin), ai1_pin(AI1_pin), stby_pin(STBY_pin), bi1_pin(BI1_pin), bi2_pin(BI2_pin), pwmb_pin(PWMB_pin){
 
             // initialize pins
-            gpio_init(dir_pin1);
-            gpio_init(dir_pin2);
+            gpio_init(ai1_pin);
+            gpio_init(ai2_pin);
+            gpio_init(bi1_pin);
+            gpio_init(bi2_pin);
             gpio_init(stby_pin);
-            gpio_set_dir(dir_pin1, GPIO_OUT);
-            gpio_set_dir(dir_pin2, GPIO_OUT);
+
+            gpio_set_dir(ai1_pin, GPIO_OUT);
+            gpio_set_dir(ai2_pin, GPIO_OUT);
+            gpio_set_dir(bi1_pin, GPIO_OUT);
+            gpio_set_dir(bi2_pin, GPIO_OUT);
             gpio_set_dir(stby_pin, GPIO_OUT);
+            
             gpio_put(stby_pin, true);
 
 
             // Setup PWM
-            gpio_set_function(pwm_pin, GPIO_FUNC_PWM);
-            uint slice_num = pwm_gpio_to_slice_num(pwm_pin);
-            pwm_set_wrap(slice_num, top);
-            pwm_set_enabled(slice_num, true);
+            gpio_set_function(pwma_pin, GPIO_FUNC_PWM);
+            gpio_set_function(pwmb_pin, GPIO_FUNC_PWM);
+            
+            pwm_set_wrap( pwm_gpio_to_slice_num(pwma_pin), top);
+            pwm_set_wrap( pwm_gpio_to_slice_num(pwmb_pin), top);
+
+            pwm_set_enabled(pwm_gpio_to_slice_num(pwma_pin), true);
+            pwm_set_enabled(pwm_gpio_to_slice_num(pwmb_pin), true);
         }
 
         //set speed [0-100%]
-        void set_speed(uint percentage) {
-            pwm_set_chan_level(pwm_gpio_to_slice_num(pwm_pin),
+        void set_speed( uint percentage, bool motor=true) {
+            // motor==true Motor A, motor==false for Motor B
+            uint &pwm_pin = (motor==true) ? pwma_pin : pwmb_pin; 
+
+            pwm_set_chan_level(pwm_gpio_to_slice_num(pwm_pin), 
             pwm_gpio_to_channel(pwm_pin),
-            percentage*top/100
-            );
+            percentage*top/100);
         }
 
-        void set_direction(string operation){
+        void set_direction(string operation, bool motor=true){
+
+            uint &dir1_pin = (motor==true) ? ai1_pin : bi1_pin; 
+            uint &dir2_pin = (motor==true) ? ai2_pin : bi2_pin;
 
             if (operation=="ccw"){
-                gpio_put(dir_pin1, false);
-                gpio_put(dir_pin2, true);
+                gpio_put(dir1_pin, false);
+                gpio_put(dir2_pin, true);
             } else if (operation=="cw"){
-                gpio_put(dir_pin1, true);
-                gpio_put(dir_pin2, false);
+                gpio_put(dir1_pin, true);
+                gpio_put(dir2_pin, false);
             } else if (operation=="short_brake"){
-                gpio_put(dir_pin1, true);
-                gpio_put(dir_pin2, true);
+                gpio_put(dir1_pin, true);
+                gpio_put(dir2_pin, true);
             } else if (operation=="stop"){
-                gpio_put(dir_pin1, false);
-                gpio_put(dir_pin2, false);
+                gpio_put(dir1_pin, false);
+                gpio_put(dir2_pin, false);
             } else {}
         }
 
     private:
-        uint pwm_pin, dir_pin1, dir_pin2, stby_pin;
-        uint top =255; // 8-bit resolution
+        uint pwma_pin, ai2_pin, ai1_pin, stby_pin, bi1_pin, bi2_pin, pwmb_pin;
+        uint top=255; // 8-bit resolution
 
 };
 
 int main(){
 
-    Motor test_motor(0,1,2,6);
+    /* Setup motors
+    Motors: 
+    Front Left(motors_left==true) 
+    Back Left(motors_left==false) 
+    Front Right(motores_right==true) 
+    Back Right(motores_right==false)
+    */
+    TB6612FNG_MD motors_left(0,1,2,6,5,4,3);
+    TB6612FNG_MD motors_right(20,21,22,19,26,27,28);
 
     while (true){
-        test_motor.set_direction("cw");
-        test_motor.set_speed(50);
-        
-        sleep_ms(5000);
 
-        test_motor.set_direction("short_brake");
-        test_motor.set_speed(10);
+        motors_left.set_direction("cw", true);
+        motors_left.set_direction("cw", false);
+        motors_right.set_direction("cw", true);
+        motors_right.set_direction("cw", false);
 
-        sleep_ms(5000);
+        motors_left.set_speed(50,true);
+        motors_left.set_speed(50,false);
+        motors_right.set_speed(50,true);
+        motors_right.set_speed(50,false);
 
-        test_motor.set_direction("ccw");
-        test_motor.set_speed(30);
-
-        sleep_ms(5000);
-
-        test_motor.set_direction("stop");
-        test_motor.set_speed(85);
 
         sleep_ms(5000);
 
-        test_motor.set_direction("cw");
-        test_motor.set_speed(85);
+        motors_left.set_direction("short_brake", true);
+        motors_left.set_direction("short_brake", false);
+        motors_right.set_direction("short_brake", true);
+        motors_right.set_direction("short_brake", false);
 
         sleep_ms(5000);
 
-        test_motor.set_direction("ccw");
-        test_motor.set_speed(0);
+        motors_left.set_direction("ccw", true);
+        motors_left.set_direction("ccw", false);
+        motors_right.set_direction("ccw", true);
+        motors_right.set_direction("ccw", false);
+
+        motors_left.set_speed(40, true);
+        motors_left.set_speed(40, false);
+        motors_right.set_speed(40, true);
+        motors_right.set_speed(40, false);
+
+
+        sleep_ms(5000);
+
+        motors_left.set_direction("stop", true);
+        motors_left.set_direction("stop", false);
+        motors_right.set_direction("stop", true);
+        motors_right.set_direction("stop", false);
+
+        sleep_ms(5000);
+
+        motors_left.set_direction("ccw", true);
+        motors_left.set_direction("ccw", false);
+        motors_right.set_direction("ccw", true);
+        motors_right.set_direction("ccw", false);
+
+        motors_left.set_speed(100, true);
+        motors_left.set_speed(100, false);
+        motors_right.set_speed(100, true);
+        motors_right.set_speed(100, false);
+
+        sleep_ms(5000);
+
+        motors_left.set_direction("cw", true);
+        motors_left.set_direction("cw", false);
+        motors_right.set_direction("cw", true);
+        motors_right.set_direction("cw", false);
+
+        motors_left.set_speed(100,true);
+        motors_left.set_speed(100,false);
+        motors_right.set_speed(100,true);
+        motors_right.set_speed(100,false);
 
         sleep_ms(5000);
     }
